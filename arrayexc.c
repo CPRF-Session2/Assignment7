46d8129b70a9a59a5c7a379a2f3474847bb8367b
ak3962 <ak3962@columbia.edu>
2016-08-01 21:48:27 -0400

    Ashank Kumar

---
+++ arrayexc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int fnx(char *string1, char *string2)
+{
+	return strcmp(string1, string2);
+}
+
+int main()
+{
+	char string1[50];
+	char string2[50];
+	int var;
+
+	printf("Enter a string:");
+	fgets(string1, 50, stdin);
+
+	printf("Enter a string:");
+	fgets(string2, 50, stdin);
+
+	char *word1 = string1;
+	char *word2 = string2;
+
+	var = fnx(word1, word2);
+
+	printf("%d\n",var);
+
+	return 0;
+}
