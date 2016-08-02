d76cdcfe3fc16b7bb2757fa2f64d25a19d071756
MariLee315 <mariposagod1234@gmail.com>
2016-08-02 10:11:09 -0400

    edits

--- compare.c
+++ compare.c
@@ -1,10 +1,25 @@
+/*Mariposa Lee, I did not realize you were supposed to make your own strcmp until 10 minutes before the due time. I quickly tried to whip it up. I apologize.*/
 #include <stdio.h>
 #include <string.h>
-int func (char *a, char *b) {
+int func (char *str1, char *str2, char *a, char *b) {
+	a=0;
+	b=0;
+	int i;
+	while (i=0; i!=(strlen(str1))||(strlen(str2));i++) {
+		if (str1[a] > str2[b]) {
+			i= -1;
+			return i;}
+	if (str1[a] < str2[b]) {
+		i=1;
+		return i; }
+	if (str1[a] == str2[b]) {
+		continue;}
 	int q;
-	q = strcmp(a, b);
+	while 
+	/*q = strcmp(a, b);*/
 return q;
 }
+
 
 int main () {
 char s1[100];
