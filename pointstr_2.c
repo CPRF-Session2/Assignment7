1ec17bc8d39fb73206a57170174fcce7f6a584c6
Aldin Aksay <afa2130@columbia.edu>
2016-08-02 08:19:02 -0400

    pointstr_2 WITH COMMENTS
    this one has comments
    
    Aldin

--- pointstr_2.c
+++ pointstr_2.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-char st1[100], st2[100], *st1_ptr, *st2_ptr; 
+char st1[100], st2[100], *st1_ptr, *st2_ptr; /*declaring my pointers*/
 int ii, nn;
-enum alphabet {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}; 
+enum alphabet {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}; /*creating an enum for the alphabet to refer to when trying to see which string is first*/
 	printf("Enter a string: ");
 	fgets(st1, 100, stdin);
 	printf("Enter another string: ");
 	fgets(st2, 100, stdin);
 	nn = strlen(st1);
-	st1_ptr = &st1;
+	st1_ptr = &st1;   /*defining the pointers*/
 	st2_ptr = &st2;
 	for (ii = 0; ii < nn - 1; ii++){
 		if (*(st1_ptr + ii) != *(st2_ptr + ii)){
-			enum alphabet letter_1; 
+			enum alphabet letter_1;    /*refers to enum of alphabet*/
 			letter_1 = *(st1_ptr + ii);
 			enum alphabet letter_2;
 			letter_2 = *(st2_ptr + ii);
@@ -28,7 +28,7 @@ enum alphabet {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v,
 		}
 		if (ii == nn - 1 && nn == strlen(st2)){
 			printf("0\n");
-			if (nn > strlen(st2))
+			if (nn > strlen(st2))            /*this doesn't work for tom and tomcat*/
 				printf("1");
 			if (nn < strlen(st2))
 				printf("-1");
